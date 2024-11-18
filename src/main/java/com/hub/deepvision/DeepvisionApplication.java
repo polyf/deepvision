2371328cf1ff313062f99927bb36ddb8d2b30ee5
Polyana Fucilini <poly.fucilini.s@gmail.com>
2024-11-18 18:15:41 -0300

    Initial commit

---
+++ src/main/java/com/hub/deepvision/DeepvisionApplication.java
@@ -0,0 +1,13 @@
+package com.hub.deepvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepvisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepvisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hub/deepvision/DeepvisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hub.deepvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepvisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
