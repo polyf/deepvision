077d042fda566197876aacfd5def9b25e4032ad1
Polyana Fucilini <poly.fucilini.s@gmail.com>
2024-11-20 23:06:23 -0300

    Correção de filtro de autenticação

--- src/main/java/com/hub/deepvision/infra/security/SecurityConfiguration.java
+++ src/main/java/com/hub/deepvision/infra/security/SecurityConfiguration.java
@@ -28,8 +28,7 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/api/auth/register").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/auth/*").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
